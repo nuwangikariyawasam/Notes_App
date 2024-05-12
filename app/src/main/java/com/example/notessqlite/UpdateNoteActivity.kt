3a60cb5cabdfeee89c6e5a8105acea396c3cc83a
nuwangikariyawasam <nuwangi2608@gmail.com>
2024-05-12 23:33:20 +0530

    last commit

--- app/src/main/java/com/example/notessqlite/UpdateNoteActivity.kt
+++ app/src/main/java/com/example/notessqlite/UpdateNoteActivity.kt
@@ -3,16 +3,16 @@ package com.example.notessqlite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.notessqlite.databinding.ActivityUpadateNoteBinding
+import com.example.notessqlite.databinding.ActivityUpdateNoteBinding
 
 class UpdateNoteActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityUpadateNoteBinding
+    private lateinit var binding: ActivityUpdateNoteBinding
     private lateinit var db: NoteDatabaseHelper
     private var noteId: Int = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUpadateNoteBinding.inflate(layoutInflater)
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
         setContentView(binding.root )
 
         db = NoteDatabaseHelper(this)
